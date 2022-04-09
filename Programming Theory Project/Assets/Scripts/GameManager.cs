57ba7085f6ad982351e27500def5ebf6df004649
s6135410018 <s6135410018@sau.ac.th>
2022-04-09 13:09:30 +0700

    title screen

---
+++ Programming Theory Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.IO;
+
+[System.Serializable]
+class SaveData
+
+{
+    public string playerName;
+
+}
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private  TMP_InputField inputname;
+    private string playerName;
+    public string PlayerName
+    {
+        get{return playerName;}
+    }
+    public static GameManager instance;
+    private void Awake() {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        inputname = GameObject.Find("InputName").GetComponent<TMP_InputField>();
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerName = inputname.text;
+        Save();
+    }
+    public void LoadScene()
+    {
+        NextScene();
+    }
+   
+    private void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    
+    private void Save()
+    {
+        SaveData data = new SaveData();
+        data.playerName = playerName;
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/saveFile.json", json);
+    }
+    public void Load()
+    {
+        string path = Application.persistentDataPath + "/saveFile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+            playerName = data.playerName;
+        }
+    }
+   
+}
--- Programming Theory Project/Assets/Scripts/MainUI.cs
+++ Programming Theory Project/Assets/Scripts/MainUI.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class MainUI : MonoBehaviour 
+using UnityEngine.SceneManagement;
+using UnityEditor;
+
+public class MainUI : MonoBehaviour
 {
     [SerializeField] GameObject profile;
-    [SerializeField] List<TextMeshProUGUI> text;
-    public static MainUI instance;
+    private TextMeshProUGUI playerShowText;
+    private string playerName;
     private void Awake()
     {
         instance = this;
+        playerShowText = GameObject.Find("playerName Text").GetComponent<TextMeshProUGUI>();
     }
+    private void Start()
+    {
+        if (GameManager.instance != null)
+        {
+            GameManager.instance.Load();
+            playerName = GameManager.instance.PlayerName;
+            playerShowText.text = $"Hello {playerName}";
+        }
+    }
+    [SerializeField] List<TextMeshProUGUI> text;
+    public static MainUI instance;
+
     public void OpenUI() //abstact
     {
         bool isActive = profile.activeSelf;
@@ -19,12 +35,27 @@ public class MainUI : MonoBehaviour
     }
     public void InfoAnimal(BaseAnimal animal)
     {
-            text[0].text = $"Name:  {animal.AnimalName}";
-            text[1].text = $"Age: {animal.Age} ";
-            text[2].text = $"Color: {animal.Color}";
-            text[3].text = $"Speed: {animal.Speed}";
-            text[4].text = $"Jump Force: {animal.JumpForce}";
-            text[5].text = $"Favorite Food: {animal.AnimalFavoriteFood()}";
+        text[0].text = $"Name:  {animal.AnimalName}";
+        text[1].text = $"Age: {animal.Age} ";
+        text[2].text = $"Color: {animal.Color}";
+        text[3].text = $"Speed: {animal.Speed}";
+        text[4].text = $"Jump Force: {animal.JumpForce}";
+        text[5].text = $"Favorite Food: {animal.AnimalFavoriteFood()}";
     }
-    
+    public void BackToMenuScene()
+    {
+        BackToMenu();
+    }
+    private void BackToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+     public void ExitGame()
+    {
+        #if (UNITY_EDITOR)
+            EditorApplication.ExitPlaymode();
+        #else
+            Application.Quit();
+        #endif
+    }
 }
